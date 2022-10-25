4d13e24f2281272d9bc29577cbfbb2f8915c4261
Victor Paranhos <victor_paranhos@hotmail.com>
2022-10-25 21:10:15 -0200

    Update function.js

--- function.js
+++ function.js
@@ -2,13 +2,15 @@ const menuMobile = document.querySelector('.menu-mobile')
 
 const Cabecalho = document.querySelector('#cabecalho')
 
+
 let valor = true
 
 menuMobile.addEventListener('click', ()=> {
   Cabecalho.classList.toggle('active')
   
-  if(valor == true){
+  if(valor){
     window.document.body.style = 'overflow: hidden'
+
     valor = false
   }else {
     window.document.body.style = 'overflow: inherit'
